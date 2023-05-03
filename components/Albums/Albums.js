a20bc7fd4780f93a56db50cc5335210ffdbaeaab
w3schools-test <aaronkaltman@gmail.com>
2023-05-03 12:52:22 -0400

    Let's try some MUI

--- components/Albums/Albums.js
+++ components/Albums/Albums.js
@@ -1,11 +1,13 @@
+import React, { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
-import Grid from '@mui/material/Grid';
 
 import client from '/apolloClient';
 
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid } from '@mui/material';
+
 const GET_ALBUMS = gql`
   query GetAlbums {
-    albums(first: 10) {
+    albums(first: 3) {
       nodes {
         albumTitle
         id
@@ -20,25 +22,49 @@ export default function Albums() {
         client: client,
     });
 
+    const [open, setOpen] = useState(false);
+    const [activeAlbumTitle, setActiveAlbumTitle] = useState('');
+
+    const handleClickOpen = (albumTitle) => {
+        setOpen(true);
+        setActiveAlbumTitle(albumTitle);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
 
     return (
         <Grid container spacing={3}>
             {data.albums.nodes.map(({ albumTitle, id, uri }) => (
-                <Grid item xs={12} sm={6} md={4} key={id}>
-                    <div>
-                        <h3>Album Title - {albumTitle}</h3>
-                        <br />
-                        <b>Album ID:</b>
-                        <p>{id}</p>
-                        <br />
-                        <b>Album URI:</b>
-                        <p>{uri}</p>
-                        <br />
-                    </div>
+                <Grid item xs={12} sm={4} key={id}>
+                    <h3>Album Title - {albumTitle}</h3>
+                    <Button variant="outlined" color="primary" onClick={() => handleClickOpen(albumTitle)}>
+                        View Details
+                    </Button>
+                    <br />
+                    <b>Album ID:</b>
+                    <p>{id}</p>
+                    <br />
+                    <b>Album URI:</b>
+                    <p>{uri}</p>
+                    <br />
                 </Grid>
             ))}
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Album Details</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>Album Title: {activeAlbumTitle}</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Grid>
     );
 }
