1ae3aaf96a38c512966b8b8229771f01af246233
w3schools-test <aaronkaltman@gmail.com>
2023-05-03 17:17:21 -0400

    Aaron Top Bar

--- components/AaronTopBar/AaronTopBar.js
+++ components/AaronTopBar/AaronTopBar.js
@@ -1,48 +1,41 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Button, Box, Container } from '@mui/material';
 
 const TopBar = () => {
     return (
         <Box sx={{ position: 'fixed', top: 0, width: '100%', zIndex: 999 }}>
-            <AppBar position="sticky" sx={{ backgroundColor: '#4caf50' }}>
-                <Toolbar>
-                    <div style={{ color: '#fff', fontWeight: 'bold' }}>
-                        One Line of Text
-                    </div>
-                    <div style={{ marginLeft: 'auto' }}>
+            <AppBar position="static" sx={{ backgroundColor: '#4caf50' }}>
+                <Container maxWidth="lg">
+                    <Toolbar
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <div
+                            style={{
+                                color: '#fff',
+                                fontWeight: 'bold',
+                                marginRight: 'auto',
+                            }}
+                        >
+                            One Line of Text
+                        </div>
                         <Button
                             variant="outlined"
-                            sx={{ color: '#fff', borderColor: '#000', borderRadius: 0 }}
+                            sx={{
+                                color: '#fff',
+                                borderColor: '#000',
+                                borderRadius: 0,
+                                marginLeft: 'auto',
+                            }}
                         >
                             Button
                         </Button>
-                    </div>
-                </Toolbar>
+                    </Toolbar>
+                </Container>
             </AppBar>
-            <Box
-                sx={{
-                    backgroundColor: '#4caf50',
-                    color: '#fff',
-                    height: '40px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    padding: '0 16px',
-                }}
-            >
-                <div style={{ fontWeight: 'bold' }}>One Line of Text</div>
-                <Button
-                    variant="outlined"
-                    sx={{
-                        color: '#fff',
-                        borderColor: '#000',
-                        borderRadius: 0,
-                        padding: '6px 12px',
-                    }}
-                >
-                    Button
-                </Button>
-            </Box>
         </Box>
     );
 };
