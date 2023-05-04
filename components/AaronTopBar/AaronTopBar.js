92d5baa29b2684905c4593586f8d3fa4d2ef6824
w3schools-test <aaronkaltman@gmail.com>
2023-05-03 21:20:38 -0400

    top bar and margin on header and margin on bar and centering

--- components/AaronTopBar/AaronTopBar.js
+++ components/AaronTopBar/AaronTopBar.js
@@ -5,7 +5,7 @@ import { AppBar, Toolbar, Button, Box, Container } from '@mui/material';
 const TopBar = () => {
     return (
         <Box sx={{ position: 'fixed', width: '100%', zIndex: 999 }}>
-            <AppBar position="sticky" sx={{ backgroundColor: '#23a25a', height: '4rem' }}>
+            <AppBar position="sticky" sx={{ backgroundColor: '#23a25a', height: '6rem' }}>
                 <Container maxWidth="xs">
                     <Toolbar
                         sx={{
