ff86dd8d32d69bfda5f2fa0fe0bfb270e241ae45
w3schools-test <aaronkaltman@gmail.com>
2023-05-03 22:16:54 -0400

    new hero2 with MUI

--- components/AaronHero2/AaronHero2.js
+++ components/AaronHero2/AaronHero2.js
@@ -1,44 +1,49 @@
 import React from 'react';
 import Image from 'next/image';
-import { Button, Grid, Typography } from '@mui/material';
-
-import styles from '/components/AaronHero2/AaronHero2.module.scss';
+import { Button, Grid, Typography, Box } from '@mui/material';
 
 const AaronHero2 = () => {
     return (
-        <section className={styles.hero}>
-            <div className={styles.container}>
-                <Grid container spacing={4}>
-                    <Grid item xs={12} md={6}>
-                        <div className={styles.imageWrapper}>
-                            <Image
-                                src="https://bpheadlesst962.wpengine.com/wp-content/uploads/2023/05/phones-app-transparent-large.webp"
-                                alt="Fixdapp way of fixing cars"
-                                width={972}
-                                height={874}
-                            />
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <div className={styles.content}>
-                            <Typography variant="h5" component="h3" className={styles.subtitle}>
-                                YOUR POCKET CAR EXPERT
-                            </Typography>
-                            <Typography variant="h2" component="h1" className={styles.title}>
-                                Save money on vehicle repairs & car mechanic bills
-                            </Typography>
-                            <Typography variant="body1" className={styles.text}>
-                                The FIXD sensor and free app tells you why your check engine light is on to help you save time and money
-                            </Typography>
-                            <div className={styles.aaronpadding}></div>
-                            <Button variant="contained" className={styles.button}>
+        <Box
+            py={8}
+            px={{ xs: 2, sm: 3, md: 4 }}
+            sx={{
+                backgroundColor: '#e5eaf5',
+                width: '100%',
+                margin: 0,
+            }}
+        >
+            <Grid container spacing={4}>
+                <Grid item xs={12} md={6}>
+                    <Box display="flex" justifyContent="center">
+                        <Image
+                            src="https://bpheadlesst962.wpengine.com/wp-content/uploads/2023/05/phones-app-transparent-large.webp"
+                            alt="Fixdapp way of fixing cars"
+                            width={972}
+                            height={874}
+                        />
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Box>
+                        <Typography variant="h5" component="h3" gutterBottom>
+                            YOUR POCKET CAR EXPERT
+                        </Typography>
+                        <Typography variant="h2" component="h1" gutterBottom>
+                            Save money on vehicle repairs & car mechanic bills
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            The FIXD sensor and free app tells you why your check engine light is on to help you save time and money
+                        </Typography>
+                        <Box py={2}>
+                            <Button variant="contained" color="primary">
                                 Get FIXD 67% Off
                             </Button>
-                        </div>
-                    </Grid>
+                        </Box>
+                    </Box>
                 </Grid>
-            </div>
-        </section>
+            </Grid>
+        </Box>
     );
 };
 
