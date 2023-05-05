cb36a9d2d462aebba3108cadcf7062b2209ebdfe
w3schools-test <aaronkaltman@gmail.com>
2023-05-04 23:09:06 -0400

    New Footer Shit

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Container, Grid, Typography,List, ListItem, ListItemText } from '@mui/material';
+import { Container, Grid, Typography, Box } from '@mui/material';
 import { styled } from '@mui/system';
 import Image from 'next/image';
 
 const Footer = styled('footer')(({ theme }) => ({
-    backgroundColor: '#e5eaf5', // Changed the background color to #e5eaf5
-    padding: theme.spacing(5, 3), // Top and bottom padding set to 40px
-    color: 'black', // Changed the text color to black
+    backgroundColor: '#e5eaf5',
+    padding: theme.spacing(5, 3),
+    color: 'black',
 }));
 
 export default function AaronFooter() {
@@ -16,76 +16,46 @@ export default function AaronFooter() {
                 <Grid container spacing={3}>
                     <Grid item xs={12} md={4}>
                         <Image
-                            src="https://bpheadlesst962.wpengine.com/wp-content/uploads/2023/05/fixdapp-logo.webp" // Replace with the path to your logo
+                            src="https://bpheadlesst962.wpengine.com/wp-content/uploads/2023/05/fixdapp-logo.webp"
                             alt="Logo"
                             width={200}
                             height={64}
                         />
                     </Grid>
                     <Grid item xs={6} sm={4} md={2}>
-                        <Typography variant="h6">ABOUT</Typography>
-                        <List>
-                            <ListItem>
-                                <ListItemText primary="About FIXD" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Careers" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Press" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Reviews" />
-                            </ListItem>
-                        </List>
+                        <Typography variant="h5">ABOUT</Typography>
+                        <Box>
+                            <Typography fontSize="1.2rem">About FIXD</Typography>
+                            <Typography fontSize="1.2rem">Careers</Typography>
+                            <Typography fontSize="1.2rem">Press</Typography>
+                            <Typography fontSize="1.2rem">Reviews</Typography>
+                        </Box>
                     </Grid>
                     <Grid item xs={6} sm={4} md={2}>
-                        <Typography variant="h6">CONTACT US</Typography>
-                        <List>
-                            <ListItem>
-                                <ListItemText primary="(925) 854-1766" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="support@fixdapp.com" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="999 Peachtree St NE Suite 840, Atlanta, GA 30309" />
-                            </ListItem>
-                        </List>
+                        <Typography variant="h5">CONTACT US</Typography>
+                        <Box>
+                            <Typography fontSize="1.2rem">(925) 854-1766</Typography>
+                            <Typography fontSize="1.2rem">support@fixdapp.com</Typography>
+                            <Typography fontSize="1.2rem">999 Peachtree St NE Suite 840, Atlanta, GA 30309</Typography>
+                        </Box>
                     </Grid>
                     <Grid item xs={6} sm={4} md={2}>
-                        <Typography variant="h6">HELP</Typography>
-                        <List>
-                            <ListItem>
-                                <ListItemText primary="Help Center" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Contact Support" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="FIXD app and sensor FAQ" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Returns and warranty" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Manage subscription" />
-                            </ListItem>
-                        </List>
+                        <Typography variant="h5">HELP</Typography>
+                        <Box>
+                            <Typography fontSize="1.2rem">Help Center</Typography>
+                            <Typography fontSize="1.2rem">Contact Support</Typography>
+                            <Typography fontSize="1.2rem">FIXD app and sensor FAQ</Typography>
+                            <Typography fontSize="1.2rem">Returns and warranty</Typography>
+                            <Typography fontSize="1.2rem">Manage subscription</Typography>
+                        </Box>
                     </Grid>
                     <Grid item xs={6} sm={4} md={2}>
-                        <Typography variant="h6">LEGAL</Typography>
-                        <List>
-                            <ListItem>
-                                <ListItemText primary="Terms of Use" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Privacy Policy" />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Advertising Disclosure" />
-                            </ListItem>
-                        </List>
+                        <Typography variant="h5">LEGAL</Typography>
+                        <Box>
+                            <Typography fontSize="1.2rem">Terms of Use</Typography>
+                            <Typography fontSize="1.2rem">Privacy Policy</Typography>
+                            <Typography fontSize="1.2rem">Advertising Disclosure</Typography>
+                        </Box>
                     </Grid>
                 </Grid>
             </Container>
