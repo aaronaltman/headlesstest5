3097aa01e3c2cfee2afbf2e16aa2e0a67922278f
w3schools-test <aaronkaltman@gmail.com>
2023-05-05 15:38:21 -0400

    Aaron Hero 2 Image - let's see what paper elevate does - more MUI

--- components/AaronHero2/AaronHero2.js
+++ components/AaronHero2/AaronHero2.js
@@ -14,47 +14,49 @@ const AaronHero2 = () => {
             }}
         >
             <div className="container">
-            <Grid container spacing={4}>
-                <Grid item xs={12} md={6}>
-                    <Paper elevation={4}>
-                    <Box display="flex" justifyContent="center">
-                        <Image
-                            src="https://bpheadlesst962.wpengine.com/wp-content/uploads/2023/05/phones-app-transparent-large.webp"
-                            alt="Fixdapp way of fixing cars"
-                            width={486} // Reduced width
-                            height={437} // Reduced height
-                            priority={true}
-                        />
-                    </Box>
-                    </Paper>
+                <Grid container spacing={4} alignItems="center">
+                    <Grid item xs={12} md={6}>
+                        <Paper elevation={4}>
+                            <Box display="flex" justifyContent="center">
+                                <Image
+                                    src="https://bpheadlesst962.wpengine.com/wp-content/uploads/2023/05/phones-app-transparent-large.webp"
+                                    alt="Fixdapp way of fixing cars"
+                                    width={486} // Reduced width
+                                    height={437} // Reduced height
+                                    priority={true}
+                                />
+                            </Box>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Paper
+                            elevation={2}
+                            sx={{
+                                backgroundColor: '#e5eaf5',
+                            }}
+                        >
+                            <Box sx={{ padding: '0 16px' }}>
+                                <Typography variant="h5" component="h3" gutterBottom>
+                                    YOUR POCKET CAR EXPERT
+                                </Typography>
+                                <Typography variant="h2" component="h1" gutterBottom>
+                                    Save money on vehicle repairs & car mechanic bills
+                                </Typography>
+                                <Typography variant="body1" gutterBottom>
+                                    The FIXD sensor and free app tells you why your check engine
+                                    light is on to help you save time and money
+                                </Typography>
+                                <Box py={2}>
+                                    <Button variant="contained" color="primary">
+                                        Get FIXD 67% Off
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                    <Paper elevation={2}
-                    sx={{
-                        backgroundColor:'#e5eaf5',
-                    }}>
-                    <Box>
-                        <Typography variant="h5" component="h3" gutterBottom>
-                            YOUR POCKET CAR EXPERT
-                        </Typography>
-                        <Typography variant="h2" component="h1" gutterBottom>
-                            Save money on vehicle repairs & car mechanic bills
-                        </Typography>
-                        <Typography variant="body1" gutterBottom>
-                            The FIXD sensor and free app tells you why your check engine light is on to help you save time and money
-                        </Typography>
-                        <Box py={2}>
-                            <Button variant="contained" color="primary">
-                                Get FIXD 67% Off
-                            </Button>
-                        </Box>
-                    </Box>
-                    </Paper>
-                </Grid>
-            </Grid>
             </div>
         </Box>
-
     );
 };
 
