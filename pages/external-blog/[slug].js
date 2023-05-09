1c6b839b83b76b0a6171304b50a95fe39a6b601b
w3schools-test <aaronkaltman@gmail.com>
2023-05-08 21:21:11 -0400

    changing slug.js no idea what I am doing

--- pages/external-blog/[slug].js
+++ pages/external-blog/[slug].js
@@ -1,47 +1,12 @@
 import { useRouter } from 'next/router';
-import { gql, useQuery } from '@apollo/client';
 
-import ExternalPost from '../../components/ExternalPost/ExternalPost';
+import ExternalPost from '/components/ExternalPost/ExternalPost.js';
 
-import otherApolloClient from '/OtherApolloClient.js'; // Import the externalClient you created earlier
-
-const GET_EXTERNAL_POST = gql`
-  query GetExternalPost($slug: ID!) {
-    post(id: $slug, idType: URI) {
-      id
-      title
-      date
-      content
-      featuredImage {
-        node {
-          sourceUrl
-        }
-      }
-      categories {
-        edges {
-          node {
-            id
-            name
-          }
-        }
-      }
-    }
-  }
-`;
-
-const ExternalPostPage = () => {
+const ExternalBlogPost = () => {
     const router = useRouter();
     const { slug } = router.query;
 
-    const { loading, error, data } = useQuery(GET_EXTERNAL_POST, {
-        variables: { slug },
-        client: otherApolloClient, // Use the externalClient you created earlier
-    });
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-
-    return <ExternalPost post={data.post} />;
+    return <ExternalPost slug={slug} />;
 };
 
-export default ExternalPostPage;
+export default ExternalBlogPost;
